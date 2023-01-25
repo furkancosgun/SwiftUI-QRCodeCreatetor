1d6d68eb20a2741392a3dfc97c60593c0a826ca5
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-25 20:26:43 +0300

    First Commit

---
+++ QRCODE-CREATOR/QRCODE_CREATORApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCODE_CREATORApp.swift
+//  QRCODE-CREATOR
+//
+//  Created by Furkan on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRCODE_CREATORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
